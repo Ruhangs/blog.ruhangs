15512d6840a19a0d3d0ca37b9b75b68d930dbd35
ruhangs <1241713147@qq.com>
2023-11-10 22:53:43 +0800

    修复路径问题

--- src/prisma/seed.ts
+++ src/prisma/seed.ts
@@ -1,4 +1,4 @@
-import { db } from "@/lib/db"
+import { db } from "../lib/db"
 
 async function main() {
   const response = await db.user.upsert({
@@ -7,7 +7,17 @@ async function main() {
     create: {
       email: '1241713147@qq.com',
       name: 'rhs',
-      password: "19981207"
+      password: "19981207",
+      profile: {
+        create: {
+          qq: "1241713247",
+          introduction: "一名热爱前端的人儿",
+          weixin: "Jan23-R",
+          github: "https://github.com/Ruhangs",
+          hometown: "重庆万州",
+          university: "重庆邮电大学"
+        }
+      }
     },
   })
   console.log(response)
